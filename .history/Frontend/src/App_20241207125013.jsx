a9a4d0b974adca0d687f0a4ec413533d18dca902
DD prog <dipanjandhar198@gmail.com>
2024-12-10 03:45:49 +0000

    Remove .history folder

--- .history/Frontend/src/App_20241207125013.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Login from "./"
-
-export const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-export default App;
--- .history/Frontend/src/App_20241207125015.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Login from "./components/Login"
-
-export const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-export default App;
